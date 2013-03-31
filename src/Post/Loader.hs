7c926fba926975602881a6bed54bec3abeaf7502
Kata <lightquake@amateurtopologist.com>
2013-03-31 00:54:26 -0500

    Pull out common post/page loader code and write a page loader.
    This is sort of hacky, and if I wanted to be able to load more things,
    I'd want to rethink it. But it works good enough for this.

--- src/Post/Loader.hs
+++ src/Post/Loader.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 -- | This is the module responsible for loading posts from disk, both
 -- the initial load and the reload triggered by file modification.
-module Post.Loader (loadPosts) where
+module Post.Loader (loadPages, loadPosts) where
 
 import           Control.Applicative       ((<$>))
 import           Control.Exception
@@ -21,16 +21,20 @@ import           Text.Pandoc               (def, readMarkdown, writeHtml)
 
 import           Post.Types
 
-
--- | Do a one-shot load of all posts off of disk. Error messages are
--- printed to stdout, but do not show up on the webpage!
-loadPosts :: FS.FilePath -- ^ Path to the directory containing post folders.
-             -> IO (Table Post)
-loadPosts dir = do
+-- | Do a one-shot load of all objects off of disk. Error messages are
+-- printed to stdout.
+loadObjects :: (Tabular a) =>
+               (FS.FilePath -> IO (Either String a))
+               -- ^ The \'loader\' function, which takes the
+               -- subdirectory.
+               -> FS.FilePath
+               -- ^ Path to the directory containing object folders.
+               -> IO (Table a)
+loadObjects loader dir = do
     subdirectories <- FS.listDirectory dir >>= filterM FS.isDirectory
     (errors, posts) <-
         (partitionEithers <$>) . forM subdirectories $ \subdir -> do
-            loaded <- loadPost subdir
+            loaded <- loader subdir
             case loaded of
                 Left errMsg -> return . Left $
                                FS.encodeString subdir ++ ": " ++ errMsg
@@ -38,20 +42,34 @@ loadPosts dir = do
     mapM_ putStrLn errors
     return $ fromList posts
 
--- | Try to load an individual post using its post.markdown and
--- meta.yml. Returns Left on parse failure or failure to laod a post,
--- or Right on success.
-loadPost :: FS.FilePath -- ^ Path to the directory containing post data.
-            -> IO (Either String Post)
-loadPost directoryPath = handle catcher $ do
-    let Right slug = FS.toText $ FS.dirname (directoryPath </> "post.markdown")
-    postText <- decodeUtf8With lenientDecode <$>
-                FS.readFile (directoryPath </> "post.markdown")
-    maybeYaml <- Yaml.decodeEither <$>
-                 FS.readFile (directoryPath </> "meta.yml")
-    return (maybeYaml >>= Yaml.parseEither (buildPost slug postText))
-    where catcher :: IOError -> IO (Either String Post)
+loadPosts :: FS.FilePath -> IO (Table Post)
+loadPosts = loadObjects $ \subdir -> loadObject buildPost
+                                     (subdir </> "post.markdown")
+                                     (subdir </> "meta.yml")
+
+loadPages :: FS.FilePath -> IO (Table Page)
+loadPages = loadObjects $ \subdir -> loadObject buildPage
+                                     (subdir </> "page.markdown")
+                                     (subdir </> "meta.yml")
+
+
+-- | Load some kind of post-y object given the files to load from and
+-- a \'builder\' function. The slug is taken from the directory name
+-- of the content.
+loadObject :: (T.Text -> T.Text -> Yaml.Object -> Yaml.Parser a)
+              -- ^ The \'builder\', which takes the slug, body, and the
+              -- yaml metadata.
+              -> FS.FilePath -- ^ Path to the content.
+              -> FS.FilePath -- ^ Path to the metadata.
+              -> IO (Either String a)
+loadObject builder contentPath metadataPath = handle catcher $ do
+    let Right slug = FS.toText . FS.dirname $ contentPath
+    objText <- decodeUtf8With lenientDecode <$> FS.readFile contentPath
+    maybeYaml <- Yaml.decodeEither <$> FS.readFile metadataPath
+    return (maybeYaml >>= Yaml.parseEither (builder slug objText))
+    where catcher :: IOError -> IO (Either String a)
           catcher err = return . Left $ show err
+
 
 -- | Build an individual post out of the 'Data.Text.Text' representing
 -- the body and the 'Data.Yaml.Object' containing the metadata.
@@ -68,3 +86,16 @@ buildPost slug body o = do
                   __postBody = writeHtml def . readMarkdown def . T.unpack
                                $ body,
                   __postPosted = posted}
+
+-- | Build a page out of the 'T.Text' representing the slug and body
+-- and the 'Yaml.Object' object containing the metadata.
+buildPage :: T.Text -> T.Text -> Yaml.Object -> Yaml.Parser Page
+buildPage slug body o = do
+    title <- o .: "title"
+    shortTitle <- o .:? "short-title" .!= title
+    return Page { __pageShortTitle = shortTitle
+                , __pageTitle = title
+                , __pageSlug = slug
+                , __pageBody = writeHtml def . readMarkdown def . T.unpack
+                               $ body
+                }

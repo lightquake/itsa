92f0c2f774b52d9b5e0f78e9ef34341671c4f273
Kata <lightquake@amateurtopologist.com>
2013-04-08 02:07:15 -0400

    Load an app-root variable in the configuration yaml.

--- src/Config.hs
+++ src/Config.hs
@@ -5,14 +5,17 @@ module Config where
 
 import Control.Applicative ((<$>), (<*>))
 import Control.Lens
-import Data.Text (Text)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text, stripSuffix)
 import Data.Time (TimeZone)
 import Data.Yaml as Yaml
 
 data Config = Config {
     __postsPerPage :: Int, -- ^ Number of posts to display on a page.
     __blogTitle :: Text, -- ^ Title of the blog.
-    __timeZone :: TimeZone -- ^ Time zone to display times in.
+    __timeZone :: TimeZone, -- ^ Time zone to display times in.
+    __appRoot :: Text -- ^ The base for all URLs used throughout,
+                      -- without the trailing slash.
 }
 
 makeLenses ''Config
@@ -21,4 +24,7 @@ instance FromJSON Config where
     parseJSON (Object o) = Config <$> o .: "posts-per-page"
                            <*> o .: "blog-title"
                            <*> fmap read (o .: "time-zone")
+                           <*> fmap stripSlash (o .: "app-root")
+      where
+        stripSlash url = fromMaybe url (stripSuffix "/" url)
     parseJSON _ = fail "Expected an object"
